da5669ed3804aa0fc8a2c92350b8119d9743f808
Chemesp <127628858+Chemesp@users.noreply.github.com>
2024-02-25 14:37:13 +0100

    Update track.py

--- backend/app/track.py
+++ backend/app/track.py
@@ -1,21 +1,6 @@
 from flask import Flask, request, jsonify
-import psycopg2
-import os
-import logging
-
-app = Flask(__name__)
-
-def get_db_connection():
-    try:
-        conn = psycopg2.connect(
-            host=os.environ['POSTGRES_HOST'],
-            database=os.environ['POSTGRES_DB'],
-            user=os.environ['POSTGRES_USR'],
-            password=os.environ['POSTGRES_PWD']
-        )
-        return conn
-    except psycopg2.DatabaseError as error:
-        logging.exception("Failed to connect to database")
+from connection import get_db_connection
+from app import app
 
 # API endpoint to track a company
 @app.route('/track', methods=['POST'])
