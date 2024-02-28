9b25e9dd5b9fcdf2cade7dc635b60742fbdd95ea
Breadifies <jomds222@gmail.com>
2024-02-28 15:07:41 +0000

    Merged

--- backend/app/api_routes.py
+++ backend/app/api_routes.py
@@ -1,3 +1,86 @@
+from flask import Blueprint, request, jsonify
+from services.AlphaVantageService import getCompanyDetails
+from connect import get_db_connection
+
+
+api_routes_blueprint = Blueprint('api_routes', __name__)
+
+# API endpoint to return company details
+@api_routes_blueprint.route('/company/<symbol>', methods = ['GET'])
+def company_details(symbol):
+
+    details = getCompanyDetails(symbol)
+
+    return details
+
+
+
+## [PROJ-11] Setup endpoint to allow searching for company names
+@api_routes_blueprint.route('/company', methods=['GET'])
+def search_companies():
+    
+    search_query = request.args.get('query', '')
+    companies = []
+    DBcompanies = getFromDB(search_query)
+    NASDAQcompanies = getFromNASDAQ(search_query, DBcompanies) #change this
+    companies = DBcompanies + NASDAQcompanies
+
+    return jsonify(companies)
+
+## Fill company page with data
+
+
+
+def getFromDB(squery):
+    try:
+        # Create SQL query which uses ILIKE to find companies which match the query
+        sql_query = """
+            SELECT CompanyName, TickerCode, Exchange
+            FROM company
+            WHERE CompanyName ILIKE %%%s%% OR TickerCode ILIKE %%%s%%
+        """
+        
+        # Connect to DB and execute query
+        # Create json data for valid companies from DB
+        conn = get_db_connection()
+        with conn.cursor() as cur:
+            cur.execute(sql_query, (squery, squery))
+            rows = cur.fetchall()
+            companies = []
+            for row in rows:
+                companies.append({
+                    'name': row[0],
+                    'ticker': row[1],
+                    'exchange': row[2],
+                    'tracked': True # Only tracked companies will exist in the database
+                    })
+    finally:
+        conn.close()
+        
+    return companies
+
+def getFromNASDAQ(squery, DBcompanies):
+    # Read the NASDAQ CSV file into a DataFrame
+    nasdaq_df = pd.read_csv('../nasdaq_listed.csv')
+
+    # Filter the DataFrame based on the search query
+    filtered_df = nasdaq_df[nasdaq_df['Name'].str.contains(squery, case=False)]
+
+    # Convert the filtered DataFrame to a list of dictionaries
+    companies = []
+    for index, row in filtered_df.iterrows():
+        # don't include comapnies already added by database
+        if row['Symbol'] not in [db_company['ticker'] for db_company in DBcompanies]:
+            companies.append({
+                'name': row['Name'],
+                'ticker': row['Symbol'],
+                'exchange': 'NASDAQ', # Default to NASDAQ since we're searching in the NASDAQ CSV
+                'tracked' : False
+                })
+        
+    return companies
+## \\ PROJ-11
+
 import pandas as pd
 from flask import Blueprint, request, jsonify
 from services.AlphaVantageService import getCompanyDetails
--- backend/app/services/AlphaVantageService.py
+++ backend/app/services/AlphaVantageService.py
@@ -10,11 +10,32 @@ from models.Article import Article
 from typing import List
 
 API_URL = "https://www.alphavantage.co"
-API_KEY = os.environ['AV_KEY']
+API_KEY = "RH7EG7YN7BPG9V7Z"
 
 class CompanyDetails:
     def __init__(self, name) -> None:
         self.name = name
+
+
+def companySearch(keyword: str) -> str:
+    endpoint = f'{API_URL}/query'
+    payload = {'function': 'SYMBOL_SEARCH', 'keywords': keyword, 'apikey': API_KEY}
+    #use symbol search to convert a string name to relevant ticker
+    #Note -> prioritises ticker symbol over anything else so popular companies may not be the first response... e.g. searching "apple" does not have Apple Inc as first response, -> in which case you must input Apple%20Inc to specify the name with %20 being space
+    response = requests.get(endpoint, params=payload)
+    if response.status_code != 200: 
+        logging.error(f'Failed Company Search. Error {response.json()}')
+        raise APIError()
+    
+    data = response.json()
+    first_match = data["bestMatches"][0] #get the first company match (if exists)
+    #remove if its type is not Equity
+    #
+    symbol=first_match["1. symbol"]
+    if (first_match["4. region"] == "United Kingdom"):
+        symbol += ".LON"
+    return symbol
+
 
 
 def getCompanyDetails(symbol: str) -> CompanyDetails:
