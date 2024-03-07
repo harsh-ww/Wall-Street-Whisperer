675871dff604f452d2b88f402ccdb51197f6dd4a
Arun Pattni <20560218+ApCoder123@users.noreply.github.com>
2024-03-07 20:46:13 +0000

    recent articles

--- backend/app/routes/article_routes.py
+++ backend/app/routes/article_routes.py
@@ -33,3 +33,22 @@ def get_articles(ticker:str):
         articles = get_articles_by_ticker_db(ticker)
 
         return jsonify(articles)
+
+def get_recent_articles():
+    conn = get_db_connection()
+    articles = []
+    with conn.cursor() as cur:
+          cur.execute("WITH Ranked AS (SELECT a.*, ca.companyID, ROW_NUMBER() OVER (PARTITION BY ca.CompanyID ORDER BY a.PublishedDate DESC, ABS(a.overallScore) DESC) AS rn FROM article a INNER JOIN company_articles ca on a.articleID=ca.articleID) SELECT r.*, c.CompanyName FROM Ranked r JOIN company c ON r.companyID=c.companyID ORDER BY rn ASC, PublishedDate DESC LIMIT 6;")
+          rows = cur.fetchall()
+          for row in rows:
+                row_dict = dict(zip([column[0] for column in cur.description], row))
+                articles.append(row_dict)
+
+    return articles
+
+@article_routes_blueprint.route('/recent-articles', methods=['GET'])
+def recent_articles():
+        #from_date = request.args.get('from_date')
+        articles = get_recent_articles()
+
+        return jsonify(articles)
