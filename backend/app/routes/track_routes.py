5c7201a4a0c24460a0db73c4cb4fb2b1c7b888b7
Arun Pattni <20560218+ApCoder123@users.noreply.github.com>
2024-03-08 18:50:39 +0000

    move to track_routes.py

--- backend/app/routes/track_routes.py
+++ backend/app/routes/track_routes.py
@@ -70,4 +70,23 @@ def track_company():
 
     return jsonify({'message': 'Company successfully tracked'}), 201
 
+@track_blueprint.route('/tracked', methods=['GET'])
+def get_tracked_companies():
+    conn = get_db_connection()
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT c.CompanyID, c.CompanyName, c.TickerCode, c.Exchange
+            FROM company c
+        """)
+                    # INNER JOIN user_follows_company ufc ON c.CompanyID = ufc.CompanyID
+        tracked_companies = [{
+            'id': row[0],
+            'name': row[1],
+            'ticker_code': row[2],
+            'exchange': row[3]
+        } for row in cur.fetchall()]
+    
+    conn.close()
+    
+    return jsonify(tracked_companies)
 
