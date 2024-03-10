6e300aa1ab25e0709aafef5cf251f13c3236f6b2
rachelhsl <108424542+rachelhsl@users.noreply.github.com>
2024-03-10 16:23:22 +0000

    suggestions fade in

--- frontend/src/components/Suggestions.tsx
+++ frontend/src/components/Suggestions.tsx
@@ -1,4 +1,4 @@
-import { Flex, Link, Text, Box } from "@chakra-ui/react";
+import { Flex, Link, Text, Box, Fade} from "@chakra-ui/react";
 
 interface Props {
   companyName: string;
@@ -7,35 +7,37 @@ interface Props {
 
 function SuggestionsGenerator({ companyName, ticker }: Props) {
   return (
-    <Flex
-      bgGradient="linear(to-r, purple.300, purple.100)"
-      h="auto"
-      justifyContent="center"
-      alignItems="center"
-      boxShadow="lg"
-      p="3"
-      rounded="xl"
-      border="2px"
-      borderColor="purple.300"
-      overflow="hidden"
-      maxW="300px"
-      maxH="120px"
-      flexDirection="column" 
-      textAlign="center"
-      transition="all 0.3s"
-      _hover={{ //on hover styling
-        transform: "scale(1.015)",
-        borderColor: "purple.400",
-        boxShadow: "xl",
-      }}
-    >
-      <Link href={`/company/${ticker}`} textDecoration="none" _hover={{ textDecoration: 'none' }}>
-        <Text fontWeight="bold" fontSize="lg" mb="2" color="gray.800">{companyName}</Text>
-      </Link>
-      <Box mt="auto"> {/* Pushes ticker to bottom */}
-        <Text fontStyle="italic" color="gray.600" fontSize="sm" lineHeight="1.2">Ticker: {ticker}</Text>
-      </Box>
-    </Flex>
+    <Fade in={true}>
+      <Flex
+        bgGradient="linear(to-r, purple.300, purple.100)"
+        h="auto"
+        justifyContent="center"
+        alignItems="center"
+        boxShadow="lg"
+        p="3"
+        rounded="xl"
+        border="2px"
+        borderColor="purple.300"
+        overflow="hidden"
+        maxW="300px"
+        maxH="120px"
+        flexDirection="column" 
+        textAlign="center"
+        transition="all 0.3s"
+        _hover={{ //on hover styling
+          transform: "scale(1.015)",
+          borderColor: "purple.400",
+          boxShadow: "xl",
+        }}
+      >
+        <Link href={`/company/${ticker}`} textDecoration="none" _hover={{ textDecoration: 'none' }}>
+          <Text fontWeight="bold" fontSize="lg" mb="2" color="gray.800">{companyName}</Text>
+        </Link>
+        <Box mt="auto"> {/* Pushes ticker to bottom */}
+          <Text fontStyle="italic" color="gray.600" fontSize="sm" lineHeight="1.2">Ticker: {ticker}</Text>
+        </Box>
+      </Flex>
+    </Fade>
   );
 }
 
